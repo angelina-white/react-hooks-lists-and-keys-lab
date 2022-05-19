8c2fbca54e250e60c31520924faef1ba88902d0e
Angelina <angelinarwhite@gmail.com>
2022-05-19 11:36:16 -0400

    match example

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,18 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  
+  const projectElements = projects.map((e) => 
+  {
+    return <ProjectItem key={e.id} name={e.name} about={e.about} technologies={e.technologies}></ProjectItem>
+  });
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projectElements}
+      </div>
     </div>
   );
 }
